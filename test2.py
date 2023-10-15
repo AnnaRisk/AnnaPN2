92695c45daa996ea3f8d57a79438924e2a6b7bb0
AnnaRisk <annarainchik@gmail.com>
2023-10-15 13:57:29 +0200

    feat: add test_first

--- test2.py
+++ test2.py
@@ -1,1 +1,18 @@
-print("Helloп")
+import requests
+import pprint
+
+from pprint import pprint
+
+
+def test_111():
+    url = "https://send-request.me/api/users/"
+    data = {
+      "first_name": "Diana",
+      "last_name": "Donovan",
+      "company_id": 2,
+      "user_id": 68
+    }
+    response = requests.post(url,json = data)
+    
+    print(response.json())
+    print(response.status_code)
